e8ae5088a14307c55cdd173a9841222737ae3ea4
itzkunal01 <kunal.sharmag12@gmail.com>
2023-03-24 09:57:10 +0530

    lottie added and name change

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Lottie from "react-lottie-player";
+import pie_lottie_1 from "../assets/lottie/pie-animation-green-diaganol-mobile.json";
 
 const SectionOne = () => {
   return (
     <>
-      <div className="min-vh-100 bg-primary d-flex align-items-center justify-content-center">
+      <div className="min-vh-100 bg-primary d-flex flex-column align-items-center justify-content-center">
         <p className="fs-1 text-white fw-semibold">Section One</p>
+        <Lottie
+          loop
+          animationData={pie_lottie_1}
+          play
+          className="w-100 h-100"
+        />
       </div>
     </>
   );
