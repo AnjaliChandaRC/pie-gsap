aec0fcfaf09037c26fdfb6e342df1ab4bfe42d09
itzkunal01 <kunal.sharmag12@gmail.com>
2023-03-24 10:22:07 +0530

    two more lotties added

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -3,30 +3,59 @@ import Lottie from "react-lottie-player";
 import pie_lottie_1 from "../assets/lottie/pie-animation-green-diaganol-mobile.json";
 import pie_lottie_2 from "../assets/lottie/pie-animation-orange-diagonal.json";
 import pie_lottie_3 from "../assets/lottie/pie-animation-pink-pie-chart-mobile.json";
+import pie_lottie_4 from "../assets/lottie/pie-animation-purple-vertical-mobile.json";
+import pie_lottie_5 from "../assets/lottie/pie-animation-red-lens-mobile.json";
 
 const SectionOne = () => {
   return (
     <>
       <div className="min-vh-100 d-flex flex-column align-items-center justify-content-center section_one">
-        <h1 className="text-dark fw-semibold">Investor Deck 2023</h1>
+        <h1 className="text-dark fw-bold">Investor Deck 2023</h1>
         <Lottie
           loop
           animationData={pie_lottie_1}
           play
           className="w-100 h-100"
         />
+        <div className="text-dark fw-semibold py-5 bg-danger w-100 text-center">
+          <h2>Separator 1</h2>
+        </div>
         <Lottie
           loop
           animationData={pie_lottie_2}
           play
           className="w-100 h-100"
         />
+        <div className="text-dark fw-semibold py-5 bg-danger w-100 text-center">
+          <h2>Separator 2</h2>
+        </div>
         <Lottie
           loop
           animationData={pie_lottie_3}
           play
           className="w-100 h-100"
         />
+        <div className="text-dark fw-semibold py-5 bg-danger w-100 text-center">
+          <h2>Separator 3</h2>
+        </div>
+        <Lottie
+          loop
+          animationData={pie_lottie_4}
+          play
+          className="w-100 h-100"
+        />
+        <div className="text-dark fw-semibold py-5 bg-danger w-100 text-center">
+          <h2>Separator 4</h2>
+        </div>
+        <Lottie
+          loop
+          animationData={pie_lottie_5}
+          play
+          className="w-100 h-100"
+        />
+        <div className="text-dark fw-semibold py-5 bg-danger w-100 text-center">
+          <h2>Separator 5</h2>
+        </div>
       </div>
     </>
   );
