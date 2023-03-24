922da877fa2f229ca9ec7850cb8f0c3133a7dcad
itzkunal01 <kunal.sharmag12@gmail.com>
2023-03-24 10:05:05 +0530

    bg-color

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -5,7 +5,7 @@ import pie_lottie_1 from "../assets/lottie/pie-animation-green-diaganol-mobile.j
 const SectionOne = () => {
   return (
     <>
-      <div className="min-vh-100 bg-primary d-flex flex-column align-items-center justify-content-center">
+      <div className="min-vh-100 d-flex flex-column align-items-center justify-content-center section_one">
         <p className="fs-1 text-white fw-semibold">Section One</p>
         <Lottie
           loop
